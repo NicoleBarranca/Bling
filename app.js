7c829477919c1e7edd87ab545303b8ced18f2fbb
Nicole Barranca <nikkibarranca@gmail.com>
2022-06-22 14:56:33 -0400

    added javascript to render product matching menu item

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const products = [
     id: 1,
     title: "Diamond Stud Earings",
     price: 198,
+    description: "This is a nice pair of earings!",
     colors: [
       {
         code: "white",
         img: "img/EARING.png",
       },
       {
-        code: "light blue",
-        img: "img/EARING.png",
+        code: "lightblue",
+        img: "img/EARING-blue.png",
       },
     ],
   },
@@ -22,14 +23,15 @@ const products = [
     id: 2,
     title: "Diamond Bracelet",
     price: 118,
+    description: "This is a nice bracelet!",
     colors: [
       {
         code: "white",
         img: "img/BRACELET.png",
       },
       {
-        code: "light pink",
-        img: "img/BRACELET.png",
+        code: "lightblue",
+        img: "img/BRACELET-blue.png",
       },
     ],
   },
@@ -37,14 +39,15 @@ const products = [
     id: 3,
     title: "Diamond Square Necklace",
     price: 178,
+    description: "This is a nice necklace!",
     colors: [
       {
         code: "white",
-        img: "img/BRACELET.png",
+        img: "img/NECKLACE.png",
       },
       {
-        code: "light yellow",
-        img: "img/BRACELET.png",
+        code: "lightyellow",
+        img: "img/NECKLACE-yellow.png",
       },
     ],
   },
@@ -52,23 +55,53 @@ const products = [
     id: 4,
     title: "Blue Stone Diamond Ring",
     price: 216,
+    description: "This is a nice ring!",
     colors: [
       {
-        code: "dark blue",
-        img: "img/BRACELET.png",
+        code: "darkblue",
+        img: "img/RRING.png",
       },
       {
-        code: "light blue",
-        img: "img/BRACELET.png",
+        code: "lightblue",
+        img: "img/Ring-lightblue.png",
       },
     ],
   },
 ];
 
+let chosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductDescription = document.querySelector(".productDesc");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
 Array.from(menuItems).forEach((item, index) => {
   item.addEventListener("click", () => {
+    // change the current slide
     // console.log(" you have clicked!" + index);
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    //change the chosen product
+    chosenProduct = products[index];
+
+    // change text of current product
+    currentProductTitle.textContent = chosenProduct.title;
+    currentProductPrice.textContent = "$" + chosenProduct.price;
+    currentProductImg.src = chosenProduct.colors[0].img;
+    currentProductDescription.textContent = chosenProduct.description;
+
+    //assign new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = chosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = chosenProduct.colors[index].img;
   });
 });
 
