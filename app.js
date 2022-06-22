bd2eb2e9f82588390bbb43f5873d6ea4b7924e61
Nicole Barranca <nikkibarranca@gmail.com>
2022-06-22 14:06:45 -0400

    created array of products

--- app.js
+++ app.js
@@ -2,6 +2,69 @@ const wrapper = document.querySelector(".sliderWrapper");
 
 const menuItems = document.getElementsByClassName("menuItem");
 
+const products = [
+  {
+    id: 1,
+    title: "Diamond Stud Earings",
+    price: 198,
+    colors: [
+      {
+        code: "white",
+        img: "img/EARING.png",
+      },
+      {
+        code: "light blue",
+        img: "img/EARING.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Diamond Bracelet",
+    price: 118,
+    colors: [
+      {
+        code: "white",
+        img: "img/BRACELET.png",
+      },
+      {
+        code: "light pink",
+        img: "img/BRACELET.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Diamond Square Necklace",
+    price: 178,
+    colors: [
+      {
+        code: "white",
+        img: "img/BRACELET.png",
+      },
+      {
+        code: "light yellow",
+        img: "img/BRACELET.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Blue Stone Diamond Ring",
+    price: 216,
+    colors: [
+      {
+        code: "dark blue",
+        img: "img/BRACELET.png",
+      },
+      {
+        code: "light blue",
+        img: "img/BRACELET.png",
+      },
+    ],
+  },
+];
+
 Array.from(menuItems).forEach((item, index) => {
   item.addEventListener("click", () => {
     // console.log(" you have clicked!" + index);
